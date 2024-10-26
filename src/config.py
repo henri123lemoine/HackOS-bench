4359af1a846de9ca4aa995dbe3f20b8d4bbad06a
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-26 08:43:15 -0400

    Increased lr

--- src/config.py
+++ src/config.py
@@ -16,12 +16,11 @@ class PretrainedConfig:
     model_class: Type[PreTrainedModel]
     processor_class: Type[PreTrainedTokenizerBase]
     num_labels: int = 2
-    learning_rate: float = 1e-5
+    learning_rate: float = 5e-5
     weight_decay: float = 0.01
     freeze_backbone: bool = True
-    # Attribute name for the backbone (e.g., 'vit' for ViT)
     backbone_attr: str = "base_model"
-    classifier_attr: str = "classifier"  # Attribute name for the classification head
+    classifier_attr: str = "classifier"
     save_path: str | Path = MODELS_PATH
     checkpoint_name: str = "best_model.pth"
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
